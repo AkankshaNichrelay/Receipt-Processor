2cbbecf759ff58036b61f818c9a7c69fd130f209
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-04-02 15:13:20 -0500

    use constants for fixed values

--- internal/receipts/constraints.go
+++ internal/receipts/constraints.go
@@ -6,6 +6,20 @@ import (
 	"time"
 )
 
+const (
+	totalIsRoundDollarPoints = 50
+	totalIsMulitplePoints    = 25
+	countItemPairsPoints     = 5
+	purchaseDateIsOddPoints  = 6
+	purchaseTimeRangePoints  = 10
+
+	totalIsMulitpleConstraintMultiplier            = 0.25
+	itemDescriptionLengthConstraintMultiplier      = 3
+	itemDescriptionLengthConstraintPriceMultiplier = 0.2
+	purchaseTimeRangeConstraintStartTime           = 1400
+	purchaseTimeRangeConstraintEndTime             = 1600
+)
+
 // receiptAlphaNumericCharsConstraintPoints 1 point for every alphanumeric character in the retailer name.
 func (r *Receipts) receiptAlphaNumericCharsConstraintPoints(retailer string) int64 {
 	var alphaNumCount int64
@@ -17,60 +31,63 @@ func (r *Receipts) receiptAlphaNumericCharsConstraintPoints(retailer string) int
 	return alphaNumCount
 }
 
-// totalIsRoundDollarConstraintPoints 50 points if the total is a round dollar
+// totalIsRoundDollarConstraintPoints returns totalIsRoundDollarPoints points if the total is a round dollar
 func (r *Receipts) totalIsRoundDollarConstraintPoints(total float64) int64 {
 	if r.isWholeNumber(total) {
-		return 50
+		return totalIsRoundDollarPoints
 	}
 	return 0
 }
 
-// totalIsMulitpleConstraintPoints 25 points if the total is a multiple of 0.25
+// totalIsMulitpleConstraintPoints returns totalIsMulitplePoints points if the total is a multiple of totalIsMulitpleConstraintMultiplier
 func (r *Receipts) totalIsMulitpleConstraintPoints(total float64) int64 {
-	rem := total / 0.25
+	rem := total / totalIsMulitpleConstraintMultiplier
 	if r.isWholeNumber(rem) {
-		return 25
+		return totalIsMulitplePoints
 	}
 	return 0
 }
 
-// countItemPairsConstraintPoints 5 points for every two items on the receipt.
+// countItemPairsConstraintPoints returns countItemPairsPoints points for every two items on the receipt.
 func (r *Receipts) countItemPairsConstraintPoints(itemLength int) int64 {
-	return 5 * (int64(itemLength) / 2)
+	return countItemPairsPoints * (int64(itemLength) / 2)
 }
 
 // TODO: check if this needs to be pointer
 /* Function: itemDescriptionLengthConstraintPoints
- * Constraint: If the trimmed length of the item description is a multiple of 3,
- * multiply the price by `0.2` and round up to the nearest integer.
+ * Constraint: If the trimmed length of the item description is a multiple of itemDescriptionLengthConstraintMultiplier,
+ * multiply the price by `itemDescriptionLengthConstraintPriceMultiplier` and round up to the nearest integer.
  * The result is the number of points earned.
 **/
 func (r *Receipts) itemDescriptionLengthConstraintPoints(items []Item) int64 {
 	points := 0.0
 	for _, item := range items {
 		itemLength := len(strings.TrimSpace(item.ShortDescription))
-		// check is item length is a multiple of 3
-		if itemLength%3 == 0 {
-			points += math.Ceil(item.Price * 0.2)
+		// check is item length is a multiple of itemDescriptionLengthConstraintMultiplier
+		if itemLength%itemDescriptionLengthConstraintMultiplier == 0 {
+			points += math.Ceil(item.Price * itemDescriptionLengthConstraintPriceMultiplier)
 		}
 	}
 
 	return int64(points)
 }
 
-// purchaseDateIsOddContraintPoints 6 points if the day in the purchase date is odd
+// purchaseDateIsOddConstraintPoints returns purchaseDateIsOddPoints points if the day in the purchase date is odd
 func (r *Receipts) purchaseDateIsOddConstraintPoints(purchaseDate time.Time) int64 {
 	if purchaseDate.Day()%2 == 1 {
-		return 6
+		return purchaseDateIsOddPoints
 	}
 	return 0
 }
 
-// purchaseTimeRangeConstraintPoints 10 points if purchaseTime is after 2:00pm and before 4:00pm
+/* purchaseTimeRangeConstraintPoints returns purchaseTimeRangePoints points if purchaseTime is
+ * after purchaseTimeRangeConstraintStartTime and before purchaseTimeRangeConstraintEndTime.
+ * Assuming the start time and end time in range are not included.
+**/
 func (r *Receipts) purchaseTimeRangeConstraintPoints(purchaseTime time.Time) int64 {
 	time24hr := purchaseTime.Hour()*100 + purchaseTime.Minute()
-	if 1400 <= time24hr && time24hr <= 1600 {
-		return 10
+	if purchaseTimeRangeConstraintStartTime < time24hr && time24hr < purchaseTimeRangeConstraintEndTime {
+		return purchaseTimeRangePoints
 	}
 	return 0
 }
