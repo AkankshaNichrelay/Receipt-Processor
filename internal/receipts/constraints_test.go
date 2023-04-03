1ff7866436efff50e1dd753c3971a84fbb04c39d
AkankshaNichrelay <kanksha.nichrelay@gmail.com>
2023-04-03 02:35:29 -0500

    added table driven tests

--- internal/receipts/constraints_test.go
+++ internal/receipts/constraints_test.go
@@ -3,7 +3,9 @@ package receipts
 import (
 	"encoding/json"
 	"log"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -32,6 +34,62 @@ const receipt2 = `
     ]
 }
 `
+const receipt3 = `
+{
+	"retailer": "Target",
+	"purchaseDate": "2022-01-01",
+	"purchaseTime": "13:01",
+	"items": [
+	  {
+		"shortDescription": "Mountain Dew 12PK",
+		"price": "6.49"
+	  },{
+		"shortDescription": "Emils Cheese Pizza",
+		"price": "12.25"
+	  },{
+		"shortDescription": "Knorr Creamy Chicken",
+		"price": "1.26"
+	  },{
+		"shortDescription": "Doritos Nacho Cheese",
+		"price": "3.35"
+	  },{
+		"shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+		"price": "12.00"
+	  }
+	],
+	"total": "35.35"
+}
+`
+
+const receipt4 = `
+{
+	"retailer": "M&M Corner Market",
+	"purchaseDate": "2022-03-20",
+	"purchaseTime": "14:33",
+	"items": [
+	  {
+		"shortDescription": "Gatorade",
+		"price": "2.25"
+	  },{
+		"shortDescription": "Gatorade",
+		"price": "2.25"
+	  },{
+		"shortDescription": "Gatorade",
+		"price": "2.25"
+	  },{
+		"shortDescription": "Gatorade",
+		"price": "2.25"
+	  }
+	],
+	"total": "9.00"
+}
+`
+
+type TableTests struct {
+	description string
+	receipt     string
+	expected    int64
+}
 
 func MockReceipts() *Receipts {
 	lg := log.Default()
@@ -39,14 +97,184 @@ func MockReceipts() *Receipts {
 	return r
 }
 
-func TestRetailerAlphaNumericCharsConstraintPoints(t *testing.T) {
+func TestRetailerAlphaNumConstraint(t *testing.T) {
 	r := MockReceipts()
-	rec1 := Receipt{}
-	err := json.Unmarshal([]byte(receipt1), &rec1)
-	if !assert.NoError(t, err) {
-		return
+
+	cases := []TableTests{
+		{description: "morning", receipt: receipt2, expected: 9},
+		{description: "readme1", receipt: receipt3, expected: 6},
+		{description: "readme2", receipt: receipt4, expected: 14},
 	}
 
-	points := r.retailerAlphaNumConstraint(rec1.Retailer)
-	assert.Equal(t, points, int64(6), "TestRetailerAlphaNumericCharsConstraintPoints failed")
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			points := r.retailerAlphaNumConstraint(rec.Retailer)
+			assert.Equal(t, tt.expected, points, "TestRetailerAlphaNumConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestTotalRoundDollarConstraint(t *testing.T) {
+	r := MockReceipts()
+
+	cases := []TableTests{
+		{description: "readme1", receipt: receipt3, expected: 0},
+		{description: "readme2", receipt: receipt4, expected: totalDollarPoints},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+			total, err := strconv.ParseFloat(rec.Total, 64)
+			if !assert.NoError(t, err) {
+				return
+			}
+			points := r.totalRoundDollarConstraint(total)
+			assert.Equal(t, tt.expected, points, "TestTotalRoundDollarConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestTotalIsMulitpleConstraint(t *testing.T) {
+	r := MockReceipts()
+
+	cases := []TableTests{
+		{description: "simple", receipt: receipt1, expected: totalIsMulitplePoints},
+		{description: "morning", receipt: receipt2, expected: 0},
+		{description: "readme1", receipt: receipt3, expected: totalIsMulitplePoints},
+		{description: "readme2", receipt: receipt4, expected: totalIsMulitplePoints},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+			total, err := strconv.ParseFloat(rec.Total, 64)
+			if !assert.NoError(t, err) {
+				return
+			}
+			points := r.totalIsMulitpleConstraint(total)
+			assert.Equal(t, tt.expected, points, "TestTotalIsMulitpleConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestItemPairsConstraint(t *testing.T) {
+	r := MockReceipts()
+
+	cases := []TableTests{
+		{description: "simple", receipt: receipt1, expected: 0},
+		{description: "morning", receipt: receipt2, expected: itemPairsPoints},
+		{description: "readme1", receipt: receipt3, expected: 2 * itemPairsPoints},
+		{description: "readme2", receipt: receipt4, expected: 2 * itemPairsPoints},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			points := r.itemPairsConstraint(len(rec.Items))
+			assert.Equal(t, tt.expected, points, "TestItemPairsConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestDescriptionLengthConstraint(t *testing.T) {
+	r := MockReceipts()
+
+	cases := []TableTests{
+		{description: "simple", receipt: receipt1, expected: 0},
+		{description: "morning", receipt: receipt2, expected: 1},
+		{description: "readme1", receipt: receipt3, expected: 6},
+		{description: "readme2", receipt: receipt4, expected: 0},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			points := r.descriptionLengthConstraint(rec.Items)
+			assert.Equal(t, tt.expected, points, "TestDescriptionLengthConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestPurchaseDateOddConstraint(t *testing.T) {
+	r := MockReceipts()
+
+	cases := []TableTests{
+		{description: "readme1", receipt: receipt3, expected: purchaseDateOddPoints},
+		{description: "readme2", receipt: receipt4, expected: 0},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			purchaseDate, err := time.Parse(PurchaseDateFormat, rec.PurchaseDate)
+			if !assert.NoError(t, err) {
+				return
+			}
+			points := r.purchaseDateOddConstraint(purchaseDate)
+			assert.Equal(t, tt.expected, points, "TestPurchaseDateOddConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestPurchaseTimeRangeConstraint(t *testing.T) {
+	r := MockReceipts()
+
+	cases := []TableTests{
+		{description: "readme1", receipt: receipt3, expected: 0},
+		{description: "readme2", receipt: receipt4, expected: purchaseTimeRangePoints},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.description, func(t *testing.T) {
+			rec := Receipt{}
+			err := json.Unmarshal([]byte(tt.receipt), &rec)
+			if !assert.NoError(t, err) {
+				return
+			}
+
+			purchaseTime, err := time.Parse(PurchaseTimeFormat, rec.PurchaseTime)
+			if !assert.NoError(t, err) {
+				return
+			}
+			points := r.purchaseTimeRangeConstraint(purchaseTime)
+			assert.Equal(t, tt.expected, points, "TestPurchaseTimeRangeConstraint failed case: %s", tt.description)
+		})
+	}
+}
+
+func TestIsAlphaNumericCharBad(t *testing.T) {
+	case1 := '/'
+	r := MockReceipts()
+	ok1 := r.isAlphaNumericChar(case1)
+	assert.Equal(t, false, ok1, "TestIsAlphaNumericChar failed case: %s", case1)
 }
